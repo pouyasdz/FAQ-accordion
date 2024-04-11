c8fa9bb980ad6006185b6499a0041833ce407eb6
unknown <qpdevil83@gmail.com>
2024-04-11 16:35:20 +0430

    complete chalange

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 function handleClick (event){
-    const hostPath = window.location.protocol +"//"+ window.location.host;
-    const iconMinus =  hostPath+"/assets/images/icon-minus.svg";
-    const iconPlus = hostPath+"/assets/images/icon-plus.svg";
+    if(event.classList.contains("plus"))
+    {
+        event.classList.remove("plus");
+        event.classList.add("minus");
+    }
+    else{
+        event.classList.remove("minus");
+        event.classList.add("plus");
+    }
 
-    if(event.src == iconPlus) event.src = iconMinus;
-    else event.src = iconPlus;
     const item =  event.parentElement.parentElement;
     const detail = item.lastElementChild;
     detail.classList.contains("hidden") ? detail.classList.remove("hidden") : detail.classList.add("hidden");
